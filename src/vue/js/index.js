f1812225946dc4955f446c30c9cd1c7583a4a93d
j.yangf <j.yangf@ctrip.com>
2019-12-03 17:38:00 +0800

    add vue

---
+++ src/vue/js/index.js
@@ -0,0 +1,30 @@
+function SelfVue (options) {
+    var self = this;
+    // 劫持data的所有的key 这里的data 类比 react 的 state
+    this.data = options.data;
+    this.methods = options.methods;
+
+    Object.keys(this.data).forEach(function(key) {
+        self.proxyKeys(key);
+    });
+
+    observe(this.data);
+    new Compile(options.el, this);
+    options.mounted.call(this); // 所有事情处理好后执行mounted函数
+}
+
+SelfVue.prototype = {
+    proxyKeys: function (key) {
+        var self = this;
+        Object.defineProperty(this, key, {
+            enumerable: false,
+            configurable: true,
+            get: function getter () {
+                return self.data[key];
+            },
+            set: function setter (newVal) {
+                self.data[key] = newVal;
+            }
+        });
+    }
+}
---
+++ src/vue/js/watcher.js
@@ -0,0 +1,27 @@
+function Watcher(vm, exp, cb) {
+    this.cb = cb; // 回调
+    this.vm = vm; // node 节点
+    this.exp = exp;
+    this.value = this.get();  // 将自己添加到订阅器的操作
+}
+
+Watcher.prototype = {
+    update: function() {
+        this.run();
+    },
+    run: function() {
+        var value = this.vm.data[this.exp];
+        var oldVal = this.value;
+        if (value !== oldVal) {
+            this.value = value;
+            this.cb.call(this.vm, value, oldVal);
+        }
+    },
+    get: function() {
+        // 依赖收集： 设置 target, 获取value触发get方法， 清除 target
+        Dep.target = this;  // 缓存自己
+        var value = this.vm.data[this.exp]  // 强制执行监听器里的get函数
+        Dep.target = null;  // 释放自己
+        return value;
+    }
+};
